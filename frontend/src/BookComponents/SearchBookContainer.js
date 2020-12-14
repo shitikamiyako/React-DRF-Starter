d9227b8154d557ed20042ea428d302711883ca32
shitikakei <shitikakei@gmail.com>
2020-12-15 05:54:08 +0900

    仮動作・仮レイアウト完了

--- frontend/src/BookComponents/SearchBookContainer.js
+++ frontend/src/BookComponents/SearchBookContainer.js
@@ -1,71 +1,80 @@
 // 統括部分。Formの実行結果のレンダリング部分とフォームの処理部分をここに定義
 import React from "react";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import axios from "axios";
 import _ from "lodash";
 
 import { Grid } from "@material-ui/core";
 
-
-import SearchBookForm from './SearchBookForm'
-import SearchBookLayout from './SearchBookLayout'
-import { GBAParams } from '../Utils/GoogleBooksAPIs'
+import SearchBookForm from "./SearchBookForm";
+import SearchBookLayout from "./SearchBookLayout";
+import { GBAParams } from "../Utils/GoogleBooksAPIs";
 
 const SearchBookContainer = () => {
+  const [books, setBooks] = useState({ items: [] });
 
-    const [bookList, setBookList] = useState()
+  const baseUrl = GBAParams.ROOT_URL;
+  console.log(baseUrl);
 
-    const baseUrl = GBAParams.ROOT_URL
-
-    const searchTitle = async(data) => {
-        const params = {
-            // 完全一致で探したい
-            q: `${GBAParams.QUERY_TITLE}"${data.title}"`,
-            Country: "JP",
-            maxResults: 40,
-            startIndex: 0,
-            printType: "books",
-        }
-
-        try {
-            const response = await axios.get(baseUrl, { params: params });
-            // const responseMap = response.data.results.map((obj) => {
-            //     return obj;
-            // })
-
-            setBookList(response)
-        } catch(error) {
-            console.log(error.response)
-        }
-
+  const searchTitle = async (data) => {
+    const params = {
+      // 完全一致で探したい
+      q: `${GBAParams.QUERY_TITLE}"${data.title}"`,
+      Country: "JP",
+      maxResults: 40,
+      startIndex: 0,
+      printType: "books",
+    };
+    console.log(params);
+    try {
+      const response = await axios.get(baseUrl, { params: params });
+      // const responseMap = response.data.map((obj) => {
+      //     return obj;
+      // })
+      console.log(response.data.items);
+      setBooks(response.data);
+      console.log(books);
+    } catch (error) {
+      console.log(error.response);
     }
+  };
 
-    let BookList = bookList
-    // let BookList = Object.values(bookList);
+  let BookList = books;
+  console.log(books);
+  // let BookList = Object.values(bookList);
 
-    // useEffectを書く
+  // useEffectを書く
 
-    return (
-        <React.Fragment>
-            <SearchBookLayout>
-                <Grid direction="row" justify="center" xs={12}>
-                    <SearchBookForm onSubmit={searchTitle}/>
-                </Grid>
-                {/* ここに検索結果を一覧表示させる */}
-                <Grid container direction="row" justify="center" alignItems="center">
-                    <Grid container item xs={12} spacing={1}>
-                        {BookList.map((book) => (
-                            <Grid items xs={12} md={4} spacing={3} key={book.id}>
-                                <p>{book.title}</p>
-                            </Grid>
-                        ))}
-                    </Grid>
+  useEffect(() => {
+    setBooks(books);
+  }, [books]);
 
-                </Grid>
-            </SearchBookLayout>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <SearchBookLayout>
+        <Grid container direction="row" justify="center">
+          <SearchBookForm onSubmit={searchTitle} />
+        </Grid>
+        {/* ここに検索結果を一覧表示させる */}
+        <Grid container direction="row" justify="center" alignItems="center">
+          <Grid container item xs={12} spacing={3}>
+            {books.items.map((book, index) => (
+              <Grid item xs={12} md={4} key={index}>
+                <img
+                  alt={`${book.volumeInfo.title} book`}
+                  src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
+                />
+                <h3>{book.volumeInfo.title}</h3>
+                <p>{book.volumeInfo.publishedDate}</p>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </SearchBookLayout>
+    </React.Fragment>
+  );
+};
 
-}
+// }
 
 export default SearchBookContainer;
--- frontend/src/BookComponents/SearchBookForm.js
+++ frontend/src/BookComponents/SearchBookForm.js
@@ -1,59 +1,65 @@
 import React from "react";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import { useForm, Controller } from "react-hook-form";
-import { ErrorMessage } from '@hookform/error-message';
-import { TextField, Button  } from "@material-ui/core";
+import { ErrorMessage } from "@hookform/error-message";
+import { TextField, Button, Grid, Box } from "@material-ui/core";
 
 const SearchBookForm = ({ onSubmit }) => {
   const { control, handleSubmit, errors } = useForm();
-  const [searchWord, setSearchWord] = useState()
-  const handleChange = e => {
+  const [searchWord, setSearchWord] = useState();
+  const handleChange = (e) => {
     setSearchWord(e.target.value);
   };
-
-  useEffect(() => {
-    searchTitle()
-    return () => {
-      cleanup
-    }
-  }, [input])
 
   return (
     <React.Fragment>
       <form onSubmit={handleSubmit(onSubmit)}>
-      <Controller
-        as={<TextField />}
-        name="title"
-        control={control}
-        rules={{
-          required: "書籍のタイトルを入力してください",
-          maxLength: {
-            value: 100,
-            message: "タイトルは100文字以内です"
-          }
-          }}
-        defaultValue=""
-        onChange={handleChange}
-      />
-      <div>
-        <ErrorMessage errors={errors} name="multipleErrorInput">
-            {({ messages }) =>
-              messages && Object.entries(messages).map(([type, message]) => (
-                <p key={type}>{message}</p>
-              ))}
-        </ErrorMessage>
-      </div>
-
-        <Controller
-          as={<Button variant="outlined" color="primary">Search</Button>}
-          name="submit"
-          control={control}
-          defaultValue=""
-          onClick={handleSubmit(onSubmit)}
-        />
+        <Grid item xs={12}>
+          <Controller
+            as={
+              <TextField
+                inputProps={{ min: 0, style: { textAlign: "center" } }}
+              />
+            }
+            name="title"
+            control={control}
+            rules={{
+              required: "書籍のタイトルを入力してください",
+              maxLength: {
+                value: 100,
+                message: "タイトルは100文字以内です",
+              },
+            }}
+            defaultValue=""
+            onChange={handleChange}
+          />
+          <div>
+            <ErrorMessage errors={errors} name="multipleErrorInput">
+              {({ messages }) =>
+                messages &&
+                Object.entries(messages).map(([type, message]) => (
+                  <p key={type}>{message}</p>
+                ))
+              }
+            </ErrorMessage>
+          </div>
+          <Box mt={1} textAlign="center">
+            <Controller
+              as={
+                <Button variant="outlined" color="primary">
+                  Search
+                </Button>
+              }
+              name="submit"
+              control={control}
+              defaultValue=""
+              onClick={handleSubmit(onSubmit)}
+            />
+          </Box>
+        </Grid>
       </form>
     </React.Fragment>
   );
 };
 
-export default SearchBookForm
+export default SearchBookForm;
