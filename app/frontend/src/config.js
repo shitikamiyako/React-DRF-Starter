e2715250eec90e643d44d074d4237a7e557e8abb
shitikakei <shitikakei@gmail.com>
2021-02-09 23:37:44 +0900

    first commit

---
+++ app/frontend/src/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDTzZDkufA31jV9_mGiEpslQg-KqXIyhw8",
+    authDomain: "react-drf-starter-b3bfc.firebaseapp.com",
+    projectId: "react-drf-starter-b3bfc",
+    storageBucket: "react-drf-starter-b3bfc.appspot.com",
+    messagingSenderId: "99727126495",
+    appId: "1:99727126495:web:71da895f13c9933167de6a",
+    measurementId: "G-FFKCXMF8EJ"
+};
