fdcb8ceb349f8a4f4fad226595cd47f5cf9cd73b
shitikakei <shitikakei@gmail.com>
2021-02-20 02:52:21 +0900

    Django側にFirebase導入の認証クラスと例外を作成

---
+++ apps/authentication/excepts/firebase_auth_exceptions.py
@@ -0,0 +1,17 @@
+from REST_framework import status
+from REST_framework.exceptions import APIExeptions
+
+class NoAuthToken(APIExeptions):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = "No authentications token provided"
+    default_code = "no_auth_token"
+
+class InvalidAuthToken(APIExeptions):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = "Invalid authentication token provided"
+    default_code = "invalid_token"
+
+class FirebaseError(APIExeptions):
+    staus_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    default_detail = "The user provided with the auth token is not valid Firebase user, it has no Firebase UID"
+    default_code = "no_firebase_uid"
